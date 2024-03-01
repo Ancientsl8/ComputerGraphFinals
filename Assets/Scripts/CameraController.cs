afd9e90e1196b9cac78ec475e5984d1cfc2527be
Ancientsl8 <61736186+Ancientsl8@users.noreply.github.com>
2024-03-02 00:21:35 +0800

    Added CameraController(WIP)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Vector3 targetPoint = Vector3.zero;
+    [SerializeField] private Transform playerPosition;
+    [SerializeField] private float moveSpeed;
+    public RbMovement rbMovement;
+    public float lookAheadDistance = 5f, lookAheadSpeed = 3f;
+    private float lookOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPoint = new Vector3(playerPosition.transform.position.x, playerPosition.transform.position.y, -1);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        //if player is in the air, update the camera
+        if (rbMovement.isGrounded)
+        {
+            targetPoint.y = playerPosition.transform.position.y;
+        }
+        
+        //Checks to see if player is moving right or left (positive is going right, negative is going left)
+        if(rbMovement.rb.velocity.x > 0)
+        {
+            //Sets the camera viewpoint to the right of the player
+            lookOffset = Mathf.Lerp(lookOffset, lookAheadDistance, lookAheadSpeed * Time.deltaTime);
+        }
+
+        if (rbMovement.rb.velocity.x < 0)
+        {
+            //Sets the camera viewpoint to the left of the player
+            lookOffset = Mathf.Lerp(lookOffset, -lookAheadDistance, lookAheadSpeed * Time.deltaTime);
+        }
+        //moves the point where the camera follows based on movement;
+        targetPoint.x = playerPosition.transform.position.x + lookOffset;
+
+        //moves camera to follow the point
+        transform.position = Vector3.Lerp(transform.position, targetPoint, moveSpeed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/RbMovement.cs
+++ Assets/Scripts/RbMovement.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class RbMovement : MonoBehaviour
 {
-    Rigidbody2D rb;
+    public Rigidbody2D rb;
     private float moveInput;
     [SerializeField]private float speed = 5;
     //ground check stuff
     [SerializeField] private Transform groundCheck;
     private float jumpForce = 5;
-    private bool isGrounded;
+    public bool isGrounded;
     public float checkRadius;
     public LayerMask whatIsGround;
     // Start is called before the first frame update
